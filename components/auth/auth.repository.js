e7f4c23c6b1897ef8714921b6d78f963d3f65cc0
Suatcan Yasan <109527592+SuatcanYasan@users.noreply.github.com>
2024-04-06 00:42:03 +0300

    release(auth): test auth

--- components/auth/auth.repository.js
+++ components/auth/auth.repository.js
@@ -5,10 +5,10 @@ const getUserByEmailAndPassword = async (body) => {
     try {
         return await pool.query(
             `select id,
+                name,
                 email,
-                first_name,
-                last_name
-             from admin_users
+                password
+             from users
              where email = $1
                and password = $2`,
             [body.email,md5(body.password)]
