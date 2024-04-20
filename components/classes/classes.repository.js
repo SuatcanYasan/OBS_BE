b33fe7ae0331dc6de672172f659c5dfd72bbbbe4
Suatcan Yasan <109527592+SuatcanYasan@users.noreply.github.com>
2024-04-20 14:11:19 +0300

    refactor(classes): columns

--- components/classes/classes.repository.js
+++ components/classes/classes.repository.js
@@ -3,9 +3,23 @@ const {pool} = require('../../utils/init')
 const getAllClasses = async (data,user) => {
     try {
         return await pool.query(
-            `select *
-             from all_classes
-             where faculty_id = $1 and class = $2`,
+            `SELECT
+                 ac.id,
+                 ac.name,
+                 COUNT(DISTINCT exams.id) AS total_exams,
+                 COUNT(DISTINCT cc.id) AS classes_count
+             FROM
+                 all_classes ac
+                     LEFT JOIN
+                 exams ON ac.id = exams.classes_id
+                     LEFT JOIN
+                 current_classes cc ON ac.id = cc.classes_id
+             WHERE
+                 ac.faculty_id = $1
+               AND ac.class = $2
+             GROUP BY
+                 ac.id;
+            `,
             [user.faculty, user.class]
 
         )
