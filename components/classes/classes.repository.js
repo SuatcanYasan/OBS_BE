e55036cdd2b883758204303bed20378f46a6e60a
Suatcan Yasan <109527592+SuatcanYasan@users.noreply.github.com>
2024-04-20 16:02:42 +0300

    refactor(classes):Added new colums for all classes repository

--- components/classes/classes.repository.js
+++ components/classes/classes.repository.js
@@ -3,22 +3,24 @@ const {pool} = require('../../utils/init')
 const getAllClasses = async (data,user) => {
     try {
         return await pool.query(
-            `SELECT
-                 ac.id,
-                 ac.name,
-                 CAST(COUNT(DISTINCT exams.id) AS INT) AS exams_count,
-                 CAST(COUNT(DISTINCT cc.id) AS INT) AS classes_count
-             FROM
-                 all_classes ac
-                     LEFT JOIN
-                 exams ON ac.id = exams.classes_id
-                     LEFT JOIN
-                 current_classes cc ON ac.id = cc.classes_id
-             WHERE
-                 ac.faculty_id = $1
+            `SELECT ac.id,
+                    ac.name,
+                    COUNT(DISTINCT exams.id)    AS total_exams,
+                    COUNT(DISTINCT cc.id)       AS classes_count,
+                    COUNT(DISTINCT contents.id) AS contents_count,
+                    COUNT(DISTINCT homeworks.id) AS homeworks_count
+             FROM all_classes ac
+                      LEFT JOIN
+                  exams ON ac.id = exams.classes_id
+                      LEFT JOIN
+                  current_classes cc ON ac.id = cc.classes_id
+                      LEFT JOIN
+                  contents ON ac.id = contents.classes_id
+                      LEFT JOIN
+                  homeworks ON ac.id = homeworks.classes_id
+             WHERE ac.faculty_id = $1
                AND ac.class = $2
-             GROUP BY
-                 ac.id;
+             GROUP BY ac.id;
             `,
             [user.faculty, user.class]
 
