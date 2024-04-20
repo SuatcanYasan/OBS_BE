0df288a00920831c9166b713ec44d5007f23c6ad
Suatcan Yasan <109527592+SuatcanYasan@users.noreply.github.com>
2024-04-20 17:06:45 +0300

    refactor(classes status): Fixed Column Name

--- components/classes/classes.repository.js
+++ components/classes/classes.repository.js
@@ -61,14 +61,14 @@ const getClassesStatus = async (data,user) => {
     try {
         return await pool.query(
             `SELECT 
-                (SELECT COUNT(1) FROM exams)                                            AS exams_count,
-                (SELECT COUNT(1) FROM homeworks)                                        AS homework_count,
-                (SELECT COUNT(1) FROM contents)                                         AS contents_count,
-                (SELECT COUNT(1) FROM current_classes)                                  AS classes_count,
-                (SELECT COUNT(1) FROM exams WHERE $1 = ANY(completed_users))             AS completed_exams_count,
-                (SELECT COUNT(1) FROM homeworks WHERE $1 = ANY(completed_users))         AS completed_homework_count,
-                (SELECT COUNT(1) FROM contents WHERE $1 = ANY(completed_users))          AS completed_contents_count,
-                (SELECT COUNT(1) FROM current_classes WHERE $1 = ANY(completed_users))   AS completed_classes_count;`,
+                (SELECT COUNT(1) FROM exams)                                            AS total_exam,
+                (SELECT COUNT(1) FROM homeworks)                                        AS total_homework,
+                (SELECT COUNT(1) FROM contents)                                         AS total_content,
+                (SELECT COUNT(1) FROM current_classes)                                  AS total_classes,
+                (SELECT COUNT(1) FROM exams WHERE $1 = ANY(completed_users))             AS completed_exams,
+                (SELECT COUNT(1) FROM homeworks WHERE $1 = ANY(completed_users))         AS completed_homework,
+                (SELECT COUNT(1) FROM contents WHERE $1 = ANY(completed_users))          AS completed_contents,
+                (SELECT COUNT(1) FROM current_classes WHERE $1 = ANY(completed_users))   AS completed_classes;`,
             [user.id]
         );
     } catch (e) {
